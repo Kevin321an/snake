845daba11d093548ce91fac7350e71940ca492d9
Kevin321an <kevin321an@gmail.com>
2016-05-06 12:52:41 -0400

    updated the missed file
    updated the missed file

---
+++ Snake/SpecialFood.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Snake
+{
+    class SpecialFood:Food
+        
+    {      
+        //shot the special food on the screen with different texture
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Resources.SpeacialFood, Position * GameManager.Tile_Size, Color.White);
+        }
+    }
+}
